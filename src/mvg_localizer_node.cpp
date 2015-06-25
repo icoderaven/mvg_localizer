a741341f9b72ac4c4b53532e6a6b06ff99d452db
Kumar Shaurya Shankar <icoderaven@gmail.com>
2015-06-25 02:01:45 -0400

    Minor modifications to build minimal example

--- src/mvg_localizer_node.cpp
+++ src/mvg_localizer_node.cpp
@@ -1,4 +1,17 @@
+/**
+ * Node to try and localize new images given a precomputed databse in MVG
+ */
 #include <ros/ros.h>
+
+//MVG imports
+#include "openMVG/sfm/sfm.hpp"
+#include "openMVG/system/timer.hpp"
+
+#include "third_party/cmdLine/cmdLine.h"
+#include "third_party/stlplus3/filesystemSimplified/file_system.hpp"
+
+using namespace openMVG;
+using namespace openMVG::sfm;
 
 int main(int argc, char **argv)
 {
